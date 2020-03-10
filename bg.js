38b64d4bbc133be5a80a46ff92b0f378aed6fb40
Andy Kim <hyungSeok.kim@outlook.com>
2020-03-10 15:01:34 +1300

    Rearrange CSS

--- bg.js
+++ bg.js
@@ -1,26 +1,26 @@
 const body = document.querySelector("body")
 const IMAGENUMBER = 7;
 
-function handleImageLoad(){
+function handleImageLoad() {
     console.log("finished");
 }
 
-function paintImage(imageNumber){
+function paintImage(imageNumber) {
     const image = new Image();
     image.src = `images/${imageNumber}.jpg`
     image.classList.add("bgImage");
     body.appendChild(image);
-    
+
 }
 
-function genRandomNumber(){
-    const random = Math.floor(Math.random() * IMAGENUMBER +1);
-    return random; 
+function genRandomNumber() {
+    const random = Math.floor(Math.random() * IMAGENUMBER + 1);
+    return random;
 }
 
-function init(){
+function init() {
     const randomNumber = genRandomNumber();
-   
+
     paintImage(randomNumber);
 }
 
--- gretting.js
+++ gretting.js
@@ -1,43 +1,43 @@
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
-    greeting =document.querySelector(".js-greetings");
+    greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
-SHOWING_CN= "showing";
+    SHOWING_CN = "showing";
 
-function saveName(text){
-    localStorage.setItem(USER_LS,text)
+function saveName(text) {
+    localStorage.setItem(USER_LS, text)
 }
 
-function handleSubmit(event){
+function handleSubmit(event) {
     event.preventDefault();
     const currentValue = input.value;
     saveName(currentValue);
     paintGreeting(currentValue);
-    
+
 }
 
-function askForName(){
+function askForName() {
     form.classList.add(SHOWING_CN);
-    form.addEventListener("submit",handleSubmit)
+    form.addEventListener("submit", handleSubmit)
 }
-function paintGreeting(text){
+function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`
 }
 
-function loadName(){
+function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null){
+    if (currentUser === null) {
         askForName();
-    }else{
-        
+    } else {
+
         paintGreeting(currentUser)
     }
 }
 
-function init(){
+function init() {
     // alert("fuck");
     loadName();
 }
